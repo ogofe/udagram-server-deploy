b4aca9dd6e37379ab1e76bc5f6bf34dccba47c81
ogofe <7thogofe@gmail.com>
2022-06-26 09:52:34 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,5 @@
+
+application url = http://joel-udagram.us-east-1.elasticbeanstalk.com/
+
+github url = https://github.com/ogofe/udagram-server-deploy
+
---
+++ www/server.js
@@ -0,0 +1,47 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8082;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    app.get('/filteredimage', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        let image_url = String(req.query.image_url);
+        if (!image_url) {
+            return res.status(400)
+                .send("A valid 'image_url' query parameter is required");
+        }
+        let path = yield util_1.filterImageFromURL(image_url);
+        return res.status(200)
+            .sendFile(path, () => util_1.deleteLocalFiles([path]));
+    }));
+    //! END @TODO1
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
